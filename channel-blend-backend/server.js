6950df0c2b782eb861135eac279b6ca48546f61c
Dhruv <dhruvg@iitbhilai.ac.in>
2024-04-12 12:12:56 +0530

    Improving product individual page

--- channel-blend-backend/server.js
+++ channel-blend-backend/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const cors = require('cors'); // Import CORS module
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
--- channel-blend-frontend/src/components/IndividualProductPage/IndividualProductPage.js
+++ channel-blend-frontend/src/components/IndividualProductPage/IndividualProductPage.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import './IndividualProductPage.css'; // Import CSS file
+import { FaStar } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
+import { IoArrowBackCircleOutline } from "react-icons/io5";
 
 const IndividualProductPage = () => {
   const { id } = useParams(); // Get the product ID from the URL parameter
@@ -24,17 +27,24 @@ const IndividualProductPage = () => {
 
   return (
     <div className="container">
+      <div>
+        <Link to={`/`} className='home-btn'> <span className='arrow'><IoArrowBackCircleOutline/></span> Home</Link>
+      </div>
       <div className="product">
-        <h1>{product.product_name}</h1>
-        <img src={product.product_image} alt={product.product_name} />
-        <p>Brand: {product.brand_name}</p>
-        <p>Price: ₹{product.price}</p>
-        <p className="product-description">Description: {product.description}</p>
-        <div className="product-rating">
-          <span className="rating-label">Rating:</span> {product.reviews.rating}
+        <div>
+          <img src={product.product_image} alt={product.product_name} />
         </div>
-        <div className="product-reviews-count">
-          <span className="reviews-count-label">Reviews Count:</span> {product.reviews.count}
+        <div className='product-details'>
+          <h1>{product.product_name}</h1>
+          <p>Brand: {product.brand_name}</p>
+          <p>Price: ₹{product.price}</p>
+          <p className="product-description">Description: {product.description}</p>
+          <div className="product-rating">
+            <span className="rating-label">Rating:</span> {product.reviews.rating} <span className='star'><FaStar /></span>
+          </div>
+          <div className="product-reviews-count">
+            <span className="reviews-count-label">Reviews Count:</span> {product.reviews.count}
+          </div>
         </div>
       </div>
     </div>
